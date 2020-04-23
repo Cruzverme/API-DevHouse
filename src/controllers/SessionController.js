cb1de41fa660a6e900c4d6bd0165a29de676c57e
Charles Pereira <charles.pereira@webjump.com.br>
2020-04-23 10:50:48 -0300

    Email Validation

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -5,11 +5,15 @@ class SessionController {
   
   async store(req, res){
     const schema = Yup.object().shape({
-      email: Yup.string().required(),
+      email: Yup.string().required().email(),
     });
     
     const { email } = req.body;
     let user = await User.findOne({ email });
+
+    if(!(await schema.isValid(req.body))){
+      return res.status(400).json({error: "Invalid Email"})
+    }
 
     if(!user){
       user = await User.create({ email });
